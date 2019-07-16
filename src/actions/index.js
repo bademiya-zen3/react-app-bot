56dc23d1349d48d433570327b22dbef0c3e7e35c
S Bademiya <00001613@zen3.com>
2019-07-16 18:54:13 +0530

    charts added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,4 @@
-import { GETBARDATA } from "../actionTypes";
 import axios from "axios";
-
-//Getting Mock data
-import { dataArr, dataArrMonthly, dataArrYearly } from "../utils/seed";
-
-//@TODO
-//make an api request here with the report duration Type and
-//send array of objects as payload
-//for development purpose, arays with required results is assumed
-export const getBarData = reportType => {
-  let payLoadData;
-  if (reportType === "lastTwoDays") payLoadData = [...dataArr];
-  else if (reportType === "monthToDate") payLoadData = [...dataArrMonthly];
-  else if (reportType === "yearly") payLoadData = [...dataArrYearly];
-
-  return {
-    type: GETBARDATA,
-    payload: payLoadData
-  };
-};
 
 export const fetchData = async apiEndPoint => {
   let { data } = await axios.get(apiEndPoint);
